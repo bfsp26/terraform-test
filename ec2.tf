360151d05783328b7c2957c45b806307dee48ddc
Bryan Simbaña <bryan-_1997@outlook.com>
2025-02-19 22:42:14 -0500

    Remove ssh key

--- ec2.tf
+++ ec2.tf
@@ -7,32 +7,10 @@ resource "aws_instance" "public_instance" {
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
   user_data              = file("scripts/test.sh")
 
-  provisioner "local-exec" {
-    command = "echo 'Instance created with IP: ${self.public_ip}' >> instance_data.txt"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "echo 'Instance destroyed with IP: ${self.public_ip}' >> instance_data.txt"
-  }
-
-  provisioner "remote-exec" {
-    inline = ["echo 'Hello world!' > ~/hello.txt"]
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("custom_key.pem")
-    }
-  }
-
   tags = {
     "Name" = each.value
   }
 
-  # lifecycle {
-  #   replace_triggered_by = [ aws_subnet.private_subnet.id ]
-  # }
 }
 
 resource "aws_instance" "monitoring_instance" {
